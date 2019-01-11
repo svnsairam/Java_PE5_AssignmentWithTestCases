5cb075f254bf73a3dd85b129b975381483bcaa1e
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-01-11 11:14:09 +0530

    Written test case for Student class

--- src/main/java/com/company/Student.java
+++ src/main/java/com/company/Student.java
@@ -26,58 +26,35 @@ public class Student {
     public String   getName(){
         return this.name;
     }
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+
+    public String toString(){
+        return this.getId()+" "+this.getName()+" "+this.getAge();
     }
 
 }
 
-class StudentSort   implements Comparator<Student>{
+class StudentSort implements Comparator<Student>{
 
     @Override
     public int compare(Student o1, Student o2) {
-            if(o1.getAge()-o2.getAge() == 0){
-                return o1.getName().compareToIgnoreCase(o2.getName());
+            if(o1.getAge() != o2.getAge()){
+                return o2.getAge()-o1.getAge();
             }else {
-                return (o1.getAge()-o2.getAge());
+                String  o1Name  =   o1.getName();
+                String  o2Name  =   o2.getName();
+                int     value   =   o1Name.compareTo(o2Name);
+                return value;
             }
     }
 }
 
 class Manifest{
 
-    public List<Student> sortStudentsBasedOnAge(){
-        Student student1    =   new Student();
-        Student student2    =   new Student();
-        Student student3    =   new Student();
-        Student student4    =   new Student();
-        Student student5    =   new Student();
-        Student student6    =   new Student();
+    public ArrayList<Student> sortStudentsBasedOnAge(ArrayList<Student>  students){
+        StudentSort sortedStudent   =   new StudentSort();
+        Collections.sort(students,sortedStudent );
 
-        student1.setAge(21);student1.setId("Boeing101");student1.setName("Sai krishna");
-        student2.setAge(25);student2.setId("Boeing102");student2.setName("Jai krishna");
-        student3.setAge(27);student3.setId("Boeing103");student3.setName("Bala krishna");
-        student4.setAge(18);student4.setId("Boeing104");student4.setName("Hari krishna");
-        student5.setAge(23);student5.setId("Boeing105");student5.setName("Sri krishna");
-        student6.setAge(23);student6.setId("Boeing106");student5.setName("Rama krishna");
-
-
-
-        List<Student>   sortedStudentList =   new ArrayList<>();
-        sortedStudentList.add(student1);
-        sortedStudentList.add(student2);
-        sortedStudentList.add(student3);
-        sortedStudentList.add(student4);
-        sortedStudentList.add(student5);
-
-        Collections.sort(sortedStudentList, new StudentSort());
-
-        return sortedStudentList;
+        return students;
 
     }
 }
---
+++ src/main/test/com/company/ManifestTest.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ManifestTest {
+
+    Manifest    manifest;
+    @Before
+    public void setUp(){
+        manifest    =   new Manifest();
+    }
+    @After
+    public void tearDown(){
+        manifest    =   null;
+    }
+
+    @Test
+    public void sortStudentsSuccess(){
+        Student student1    =   new Student();
+        Student student2    =   new Student();
+        Student student3    =   new Student();
+        Student student4    =   new Student();
+        Student student5    =   new Student();
+        Student student6    =   new Student();
+
+        student1.setAge(21);student1.setId("Boeing101");student1.setName("Sai krishna");
+        student2.setAge(25);student2.setId("Boeing102");student2.setName("Jai krishna");
+        student3.setAge(27);student3.setId("Boeing103");student3.setName("Bala krishna");
+        student4.setAge(18);student4.setId("Boeing104");student4.setName("Hari krishna");
+        student5.setAge(23);student5.setId("Boeing105");student5.setName("Sri krishna");
+        student6.setAge(23);student6.setId("Boeing106");student6.setName("Rama krishna");
+
+        ArrayList<Student> expectedValue =   new ArrayList<>();
+        expectedValue.add(student3);
+        expectedValue.add(student2);
+        expectedValue.add(student6);
+        expectedValue.add(student5);
+        expectedValue.add(student1);
+        expectedValue.add(student4);
+
+        ArrayList<Student> inputStudents =   new ArrayList<>();
+        inputStudents.add(student1);
+        inputStudents.add(student2);
+        inputStudents.add(student3);
+        inputStudents.add(student4);
+        inputStudents.add(student5);
+        inputStudents.add(student6);
+
+        ArrayList<Student>   actualValue =   manifest.sortStudentsBasedOnAge(inputStudents);
+
+        Assert.assertEquals(expectedValue,actualValue);
+    }
+
+}
